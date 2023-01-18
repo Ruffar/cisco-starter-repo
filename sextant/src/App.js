f706a94930b47e97bbc53c735cdc462239ac497a
Ruffar <raffier.utomo@gmail.com>
2023-01-18 21:09:50 +0000

    task2

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import icon from './icon.svg';
+import React from 'react';
 import './App.css';
 
+
+
+class PageButton extends React.Component
+{
+  render()
+  {
+    return (
+      <button key={this.props.text+"PageButton"} className="PageButton">{this.props.text}</button>
+    );
+  }
+}
+
+class Banner extends React.Component
+{
+  render()
+  {
+    return (
+      <div className = "Banner">
+        <div className="BannerLogo">
+          <img src={icon} className="AppIcon" alt="icon"/>
+          Sextant
+        </div>
+        <PageButton text="Home" link="/"></PageButton>
+        <PageButton text="About" link="/about"></PageButton>
+      </div>
+    );
+  }
+}
+
+class Exhibit extends React.Component
+{
+  render()
+  {
+    return (
+      <div className="Exhibit">
+        <h className="ExhibitHeader">{this.props.header}</h>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner></Banner>
+      <div className="BottomBar">
+        <div>Icon made from <a href="http://www.onlinewebfonts.com/icon">Icon Fonts</a> is licensed by CC BY 3.0</div>
+      </div>
+      <div className="ExhibitContainer">
+        <Exhibit header="Public IP"></Exhibit>
+      </div>
     </div>
   );
 }
